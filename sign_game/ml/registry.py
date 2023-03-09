f960a9920db549e43e740271e3d22d9638d5e794
Chris Snowden <chrissnowden35@gmail.com>
2023-03-09 15:55:32 +1100

    Update model path and copy in docker file:

--- sign_game/ml/registry.py
+++ sign_game/ml/registry.py
@@ -2,6 +2,6 @@ from tensorflow.keras import Model, models
 import os
 
 def load_model(model_name='baseline_model')-> Model:
-    model_directory=os.path.join('/home/wendyto/code/cpsnowden/sign-game-server/models/',model_name)
+    model_directory=os.path.join('/models/',model_name)
     latest_model=models.load_model(model_directory)
     return latest_model
