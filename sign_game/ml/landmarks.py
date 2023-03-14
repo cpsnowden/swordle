1d2282f159b20d56ee770c46a4b3336b640dc891
Chris Snowden <chrissnowden35@gmail.com>
2023-03-14 18:23:20 +1100

    Ensure landmarks dict is none when no hand

--- sign_game/ml/landmarks.py
+++ sign_game/ml/landmarks.py
@@ -6,11 +6,13 @@ import pandas as pd
 import os
 import numpy as np
 
+
 class Landmarks():
-    def __init__ (self):
-        self.mp_hands = mp.solutions.hands # hands model
-        self.mp_drawing = mp.solutions.drawing_utils # Drawing utilities
-        self.model = self.mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_hands=1)
+    def __init__(self):
+        self.mp_hands = mp.solutions.hands  # hands model
+        self.mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
+        self.model = self.mp_hands.Hands(
+            min_detection_confidence=0.5, min_tracking_confidence=0.5, max_num_hands=1)
 
     def image_to_landmark(self, frame, draw_landmarks=False):
 
@@ -24,11 +26,15 @@ class Landmarks():
         return frame, landmark_object
 
     def __mediapipe_detection(self, image):
-        rbg_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # CV2 gets image as BGR, this converts it to RGB
-        rbg_image.flags.writeable = False # Locks write on image so that nobody can change the image while we process
-        results = self.model.process(rbg_image) # This uses mediapipe to detect
+        # CV2 gets image as BGR, this converts it to RGB
+        rbg_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        # Locks write on image so that nobody can change the image while we process
+        rbg_image.flags.writeable = False
+        # This uses mediapipe to detect
+        results = self.model.process(rbg_image)
         rbg_image.flags.writeable = True
-        bgr_image = cv2.cvtColor(rbg_image, cv2.COLOR_RGB2BGR) # Converts it back to BGR
+        # Converts it back to BGR
+        bgr_image = cv2.cvtColor(rbg_image, cv2.COLOR_RGB2BGR)
         return results, bgr_image
 
     def __draw_landmarks(self, image, results):
@@ -42,8 +48,8 @@ class Landmarks():
         return image
 
     def __get_landmark_object(self, results):
-        landmark_object = {}
         if results.multi_hand_landmarks:
+            landmark_object = {}
             detected_hand = results.multi_hand_landmarks[0]
             for handmark in self.mp_hands.HandLandmark:
                 landmark = detected_hand.landmark[handmark]
@@ -53,7 +59,7 @@ class Landmarks():
                 landmark_object[name+'_y'] = landmark.y
                 landmark_object[name+'_z'] = landmark.z
 
-        return landmark_object
+            return landmark_object
 
     def __get_landmark_np_array(self, results):
         if results.multi_hand_landmarks:
