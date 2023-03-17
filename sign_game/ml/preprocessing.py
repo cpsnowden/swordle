6a065a4b73630cc3ec4fa124a9daaf5a6d82e847
Chris Snowden <chrissnowden35@gmail.com>
2023-03-17 13:39:15 +1100

    Single landmark object per request

--- sign_game/ml/preprocessing.py
+++ sign_game/ml/preprocessing.py
@@ -4,8 +4,6 @@ import pandas as pd
 from sign_game.ml.landmarks import Landmarks
 from sign_game.ml.landmarks_utils import normalize_handmarks_per_image
 
-landmark = Landmarks()
-
 
 class NoHandDetectedError(Exception):
     """
@@ -17,10 +15,12 @@ class NoHandDetectedError(Exception):
 def frames_to_landmarks(frames) -> np.ndarray:
 
     frames_landmarks = []
+    landmark = Landmarks()
     for frame in frames:
         _, landmarks = landmark.image_to_landmark(frame)
         if landmarks is not None:
-            norm_landmark = normalize_handmarks_per_image(pd.DataFrame.from_dict([landmarks]))
+            norm_landmark = normalize_handmarks_per_image(
+                pd.DataFrame.from_dict([landmarks]))
             landmarks = norm_landmark.to_numpy()
             frames_landmarks.append(landmarks.flatten())
         else:
